f29ae2a5a59fec573da646b18251c0e795007a0a
Alex <sarmirim@gmail.com>
2021-07-09 19:08:03 +0300

    add useSelector

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,11 +1,15 @@
-import { Layout, Menu} from 'antd'
+import { Layout, Menu } from 'antd'
+import { useSelector } from 'react-redux'
+
 const { Header } = Layout
 
-function MyHeader()  {
+function MyHeader() {
+    const theme = useSelector((state) => state.theme.theme)
+
     return <>
         <Header className="header">
             <div className="logo" />
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu theme={theme} mode="horizontal" defaultSelectedKeys={['2']}>
             <Menu.Item key="1">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
