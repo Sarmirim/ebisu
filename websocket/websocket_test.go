1b7381c9ea2f47e799f710eaf8f10b6801a07ef6
Alex <sarmirim@gmail.com>
2021-07-27 19:59:39 +0300

    add websocket test

---
+++ websocket/websocket_test.go
@@ -0,0 +1,13 @@
+package websocket_test
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	// var c = new(websocket.WSController)
+	// go c.Start()
+	// time.Sleep(6 * time.Second)
+	// c.Stop()
+	// log.Print("TestMain done")
+}
